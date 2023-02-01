9f053bc694aaeba2ae0edc627a0d2923f7450f2b
paulocaetano96 <paulocaetano96@gmail.com>
2023-02-01 20:20:22 +0000

    memory variables update

--- scripts/highscore.js
+++ scripts/highscore.js
@@ -2,3 +2,44 @@
 document.getElementById("home-page-button").onclick = () => {
     location.href = "../index.html"
 }
+
+
+/* let txt1 = document.getElementById("player-name");   */ /* input id */
+/* let btn1 = document.getElementById("begin-game");  */      /* button id */
+/* let out1 = document.getElementById("first-place-name");  */    /* where i want the data to be at, id */
+
+/* 
+function fun1 () {
+    out1.innerHTML = txt1.value;
+}
+
+btn1.addEventListener("click", fun1);
+
+ */
+
+window.addEventListener('load', () => {
+
+    const params = (new URL(document.location)).searchParams;
+    const name
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
